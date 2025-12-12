3d619b3f99dfc72b8111161efb1065a4bfa5eff8
shubhamimsr <shubhamraskar.imsr@gmail.com>
2025-12-12 19:38:04 +0530

    updated CorsConfig class

--- src/main/java/com/imsr/expense_tracker/config/CorsConfig.java
+++ src/main/java/com/imsr/expense_tracker/config/CorsConfig.java
@@ -3,9 +3,11 @@ package com.imsr.expense_tracker.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
 
     @Value("${frontend.url}")
@@ -17,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(frontendUrl)
+                        .allowedOriginPatterns(frontendUrl)
                         .allowedMethods("GET","POST","PUT","DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
