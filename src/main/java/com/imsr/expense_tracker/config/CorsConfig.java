6180ab0c0b9b166ef8411fbb14ca635750c3592c
shubhamimsr <shubhamraskar.imsr@gmail.com>
2025-12-12 19:49:58 +0530

    updated CorsConfig class:v2

--- src/main/java/com/imsr/expense_tracker/config/CorsConfig.java
+++ src/main/java/com/imsr/expense_tracker/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.imsr.expense_tracker.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,20 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Value("${frontend.url}")
-    private String frontendUrl ;
+    private String frontendUrl;
 
-    @Bean
-    public WebMvcConfigurer corsConfig() {
+    @Bean(name = "customCorsConfigurer") // <-- FIXED
+    public WebMvcConfigurer customCorsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOriginPatterns(frontendUrl)
-                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
-
 }
